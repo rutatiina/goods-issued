3a516c14bf8274316c2950bda3841b2bccf998b9
rutatiina <rutatiina@ruginem.org>
2022-09-02 11:08:50 +0300

    Append inventory_tracking on items model

--- src/Models/GoodsIssuedItem.php
+++ src/Models/GoodsIssuedItem.php
@@ -24,6 +24,10 @@ class GoodsIssuedItem extends Model
 
     protected $guarded = ['id'];
 
+    protected $appends = [
+        'inventory_tracking',
+    ];
+
     /**
      * The "booting" method of the model.
      *
@@ -39,6 +43,11 @@ class GoodsIssuedItem extends Model
     public function item()
     {
         return $this->belongsTo('Rutatiina\Item\Models\Item', 'item_id');
+    }
+
+    public function getInventoryTrackingAttribute()
+    {
+        return optional($this->item)->inventory_tracking;
     }
 
 }
