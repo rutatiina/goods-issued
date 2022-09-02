22cd637879e14d2dd58c155468f63134c2b31a68
rutatiina <rutatiina@ruginem.org>
2022-09-02 11:55:14 +0300

    FIx bug on editing

--- src/Models/GoodsIssuedItem.php
+++ src/Models/GoodsIssuedItem.php
@@ -28,6 +28,10 @@ class GoodsIssuedItem extends Model
         'inventory_tracking',
     ];
 
+    protected $casts = [
+        'item_id' => 'integer',
+    ];
+
     /**
      * The "booting" method of the model.
      *
